fcdd2fb433837c8a561250051ba8daa17345270a
Magnus Holm <judofyr@gmail.com>
2018-09-21 17:55:15 +0200

    Ippon::Validate: Initial implementation

---
+++ lib/ippon/validate.rb
@@ -0,0 +1,334 @@
+require 'ippon'
+
+module Ippon::Validate
+
+  Error = Struct.new(:path, :message)
+
+  class Result
+    attr_accessor :value
+    attr_reader :errors, :path
+
+    def initialize(value)
+      @value = value
+      @is_halted = false
+      @errors = []
+      @path = [].freeze
+    end
+
+    def error?
+      @errors.any?
+    end
+
+    def success?
+      !error?
+    end
+
+    alias valid? success?
+
+    def halted?
+      @is_halted
+    end
+
+    def halt
+      @is_halted = true
+      self
+    end
+
+    def unhalt
+      @is_halted = false
+      self
+    end
+
+    def push_path(key)
+      @path = [*@path, key].freeze
+      self
+    end
+
+    def add_error(message)
+      @errors << Error.new(@path, message)
+      self
+    end
+
+    def add_errors_from(result)
+      @errors.concat(result.errors)
+      self
+    end
+
+    private
+
+    def initialize_copy(source)
+      @errors = source.errors.dup
+    end
+  end
+
+  class Schema
+    def validate(value)
+      result = Result.new(value)
+      process(result)
+      result
+    end
+
+    def validate!(value)
+      result = validate(value)
+      raise if result.error?
+      result.value
+    end
+
+    def |(other)
+      Sequence.new(self, other)
+    end
+
+    def &(other)
+      Merge.new(self, other)
+    end
+
+    def unhalt
+      Unhalt.new(self)
+    end
+  end
+
+  class Unhalt < Schema
+    def initialize(child)
+      @child = child
+    end
+
+    def process(result)
+      @child.process(result)
+      result.unhalt
+    end
+  end
+
+  class Sequence < Schema
+    def initialize(left, right)
+      @left = left
+      @right = right
+    end
+
+    def process(result)
+      @left.process(result)
+      return if result.halted?
+      @right.process(result)
+    end
+  end
+
+  class Step < Schema
+    attr_reader :props
+
+    def initialize(props = {})
+      @props = props.freeze
+    end
+
+    # :nocov:
+    def transform(obj)
+      obj
+    end
+
+    def valid?(obj)
+      true
+    end
+    # :nocov:
+
+    def process(result)
+      if valid?(result.value)
+        result.value = transform(result.value)
+      else
+        result.halt
+        result.add_error(message)
+      end
+      result
+    end
+
+    def message
+      @props.fetch(:message) { self.class.default_message }
+    end
+  end
+
+  class Field < Step
+    def transform(value)
+      value[props.fetch(:key)]
+    end
+  end
+
+  class Trim < Step
+    def transform(value)
+      if value
+        value = value.strip
+        value = nil if value.empty?
+      end
+      value
+    end
+  end
+
+  class Required < Step
+    def valid?(value)
+      !value.nil?
+    end
+
+    def self.default_message
+      "is required"
+    end
+  end
+
+  class Optional < Step
+    def process(result)
+      if result.value.nil?
+        result.halt
+      end
+    end
+  end
+
+  class Boolean < Step
+    def transform(value)
+      !!value
+    end
+  end
+
+  class Integer < Step
+    def valid?(obj)
+      obj =~ /\A[+-]?\d+\z/
+    end
+
+    def self.default_message
+      "must be an integer"
+    end
+
+    def transform(obj)
+      obj.to_i
+    end
+  end
+
+  class Match < Step
+    def predicate
+      @props.fetch(:predicate)
+    end
+
+    def valid?(value)
+      predicate === value
+    end
+
+    def self.default_message
+      "must match"
+    end
+  end
+
+  class Transform < Step
+    def handler
+      @props.fetch(:handler)
+    end
+
+    def process(result)
+      result.value = handler.call(result.value)
+    end
+  end
+
+  class Halt < Step
+    def predicate
+      @props.fetch(:predicate)
+    end
+
+    def process(result)
+      if predicate === result.value
+        result.halt
+      end
+    end
+  end
+
+  class Form < Schema
+    attr_reader :fields
+
+    def initialize(fields)
+      @fields = fields
+    end
+
+    def process(result)
+      values = {}
+
+      # Process all fields:
+      results = @fields.map do |key, field|
+        field_result = result.dup.push_path(key)
+        field.process(field_result)
+        [key, field_result]
+      end
+
+      # Propgate state:
+      results.each do |key, field_result|
+        if field_result.halted?
+          result.halt
+        else
+          values[key] = field_result.value
+        end
+        
+        result.add_errors_from(field_result)
+      end
+
+      result.value = values
+    end
+  end
+
+  class Merge < Schema
+    def initialize(left, right)
+      @left = left
+      @right = right
+    end
+
+    def process(result)
+      left_result = result.dup
+      right_result = result.dup
+
+      @left.process(left_result)
+      @right.process(right_result)
+
+      result.add_errors_from(left_result)
+      result.add_errors_from(right_result)
+
+      result.value = {}
+      result.value.update(left_result.value) if !left_result.halted?
+      result.value.update(right_result.value) if !right_result.halted?
+      result.halt if left_result.halted? || right_result.halted?
+      result
+    end
+  end
+
+  module Builder
+    def field(key, **props)
+      Field.new(key: key, **props)
+    end
+
+    def trim(**props)
+      Trim.new(**props)
+    end
+
+    def required(**props)
+      Required.new(**props)
+    end
+
+    def optional(**props)
+      Optional.new(**props)
+    end
+
+    def integer(**props)
+      Integer.new(**props)
+    end
+
+    def form(fields)
+      Form.new(fields)
+    end
+
+    def halt_if(**props, &blk)
+      Halt.new(predicate: blk, **props)
+    end
+
+    def match(predicate, **props)
+      Match.new(predicate: predicate, **props)
+    end
+
+    def match_with(**props, &blk)
+      match(blk, **props)
+    end
+
+    def transform(**props, &blk)
+      Transform.new(handler: blk, **props)
+    end
+  end
+end
+
---
+++ test/test_validate.rb
@@ -0,0 +1,216 @@
+require_relative 'helper'
+
+require 'ippon/validate'
+
+class TestValidate < Minitest::Test
+  include Ippon::Validate
+  include Builder
+
+  def test_basic_field
+    field = field("name") | trim | required
+
+    result = field.validate({"name" => "  Magnus "})
+    assert result.success?
+    assert_equal "Magnus", result.value
+
+    result = field.validate({"name" => "   "})
+    assert result.error?
+
+    error = result.errors[0]
+    assert_equal [], error.path
+    assert_equal "is required", error.message
+  end
+
+  def test_helpers
+    schema = field("name", message: "yes")
+    assert_instance_of Field, schema
+    assert_equal "name", schema.props[:key]
+    assert_equal "yes", schema.props[:message]
+
+    schema = form(a: field("a"))
+    assert_instance_of Form, schema
+    assert_equal [:a], schema.fields.keys
+
+    schema = transform { |val| val * 2 }
+    assert_instance_of Transform, schema
+    assert schema.props[:handler]
+
+    schema = field("name") | required
+    assert_instance_of Sequence, schema
+
+    schema = form(a: field("a")) & form(b: field("b"))
+    assert_instance_of Merge, schema
+
+    schema = halt_if { |val| val % 2 == 0 }
+    assert_instance_of Halt, schema
+  end
+
+  def test_unhalt
+    schema = match(1..20).unhalt | match_with { |val| val % 2 == 0 }
+
+    result = schema.validate(55)
+    assert result.error?
+    assert_equal 2, result.errors.size
+
+    result = schema.validate(54)
+    assert result.error?
+    assert_equal 1, result.errors.size
+
+    result = schema.validate(15)
+    assert result.error?
+    assert_equal 1, result.errors.size
+
+    schema.validate!(10)
+  end
+
+  def test_field_with_hash
+    fetch = Field.new(key: "name")
+    assert_equal "Magnus", fetch.validate!({"name" => "Magnus"})
+  end
+
+  def test_field_custom
+    fetch = Field.new(key: "name")
+    obj = Object.new
+    def obj.[](key)
+      key.upcase
+    end
+
+    assert_equal "NAME", fetch.validate!(obj)
+  end
+
+  def test_trim
+    trim = Trim.new
+
+    assert_equal "123", trim.validate!("123")
+    assert_equal "123", trim.validate!("  123  ")
+    assert_equal "1 2 3", trim.validate!("  1 2 3  ")
+    assert_nil trim.validate!("\t  \t ")
+  end
+
+  def test_optional
+    optional = Optional.new
+
+    result = optional.validate("123")
+    refute result.halted?
+
+    result = optional.validate("")
+    refute result.halted?
+
+    result = optional.validate(nil)
+    assert result.halted?
+  end
+
+  def test_integer
+    integer = Integer.new
+
+    assert_equal 123, integer.validate!("123")
+    assert_equal -123, integer.validate!("-123")
+    assert_equal 123, integer.validate!("+123")
+
+    result = integer.validate("  123  ")
+    assert result.error?
+    assert_equal "must be an integer", result.errors[0].message
+  end
+
+  def test_boolean
+    boolean = Boolean.new
+
+    assert_equal false, boolean.validate!(nil)
+    assert_equal false, boolean.validate!(false)
+    assert_equal true,  boolean.validate!("")
+    assert_equal true,  boolean.validate!("1")
+    assert_equal true,  boolean.validate!("false")
+  end
+
+  def test_match
+    match = Match.new(predicate: 123)
+
+    assert_equal 123, match.validate!(123)
+    result = match.validate(124)
+    assert result.error?
+  end
+
+  def test_match_custom
+    obj = Object.new
+    def obj.===(other)
+      (other % 2) == 0
+    end
+    match = Match.new(predicate: obj)
+
+    assert_equal 124, match.validate!(124)
+    result = match.validate(125)
+    assert result.error?
+  end
+
+  def test_halt
+    obj = Object.new
+    def obj.===(other)
+      (other % 2) == 0
+    end
+    halt = Halt.new(predicate: obj)
+
+    result = halt.validate(12)
+    assert result.valid?
+    assert result.halted?
+
+    result = halt.validate(13)
+    assert result.valid?
+    refute result.halted?
+  end
+
+  def test_transform
+    transform = Transform.new(handler: proc { |val| val * 2 })
+
+    assert_equal 4, transform.validate!(2)
+  end
+
+  def test_form
+    form = Form.new(
+      name: field("name") | trim | required,
+      bio: field("bio") | trim,
+      karma: field("karma") | trim | optional | integer,
+    )
+
+    value = form.validate!(
+      "name" => "Magnus",
+      "bio" => "Programmer  ",
+      "karma" => " 4"
+    )
+
+    assert_equal({name: "Magnus", bio: "Programmer", karma: 4}, value)
+
+    result = form.validate(
+      "name" => "  ",
+      "bio" => "Programmer  ",
+      "karma" => " abc"
+    )
+    assert result.error?
+    assert_equal 2, result.errors.size
+
+    err1, err2 = result.errors
+    assert_equal [:name], err1.path
+    assert_equal [:karma], err2.path
+  end
+
+  def test_merge
+    form1 = form(
+      name: field("name") | trim | required,
+      bio: field("bio") | trim,
+    )
+
+    form2 = form(
+      karma: field("karma") | trim | optional | integer,
+    )
+
+    schema = Merge.new(form1, form2)
+
+    value = schema.validate!(
+      "name" => "Magnus",
+      "bio" => "Programmer  ",
+      "karma" => " 4"
+    )
+
+    assert_equal({name: "Magnus", bio: "Programmer", karma: 4}, value)
+  end
+end
+
