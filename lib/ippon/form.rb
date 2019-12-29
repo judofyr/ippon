bca1a4ff057759f229bd02b3d14b6750c05da2d7
Magnus Holm <judofyr@gmail.com>
2019-12-29 14:02:35 +0100

    Start working on Ippon::Form

---
+++ lib/ippon/form.rb
@@ -0,0 +1,238 @@
+require 'ippon'
+require 'ippon/validate'
+
+module Ippon::Form
+  class Entry
+    class << self
+      def options
+        @options ||= {}
+      end
+    end
+
+    def self.[](**options)
+      _subclass_cache[options] ||= Class.new(self) {
+        @options = options
+        self
+      }
+    end
+
+    def self._subclass_cache
+      @_subclass_cache ||= {}
+    end
+
+    attr_reader :key
+
+    def initialize(key)
+      @key = key
+      setup
+    end
+
+    def setup
+    end
+  end
+
+  class Text < Entry
+    attr_accessor :value
+
+    def setup
+      @value = ""
+    end
+
+    def from_input(input)
+      @value = input.fetch(key) { "" }
+    end
+
+    def serialize
+      yield key.to_s, @value
+    end
+
+    def validate
+      ::Ippon::Validate::Result.new(@value)
+    end
+  end
+
+  class TextList < Entry
+    attr_accessor :values
+
+    def setup
+      @values = []
+    end
+
+    def from_input(input)
+      @values = input.fetch_all(key)
+    end
+
+    def serialize
+      @values.each do |value|
+        yield key.to_s, value
+      end
+    end
+
+    def validate
+      ::Ippon::Validate::Result.new(@values)
+    end
+
+    def each(&blk)
+      @values.each(&blk)
+    end
+
+    include Enumerable
+  end
+
+  class Flag < Entry
+    attr_writer :checked
+
+    def setup
+      @checked = nil
+    end
+
+    def checked?
+      @checked
+    end
+
+    def from_input(input)
+      if value = input[key]
+        @checked = (value == "1")
+      end
+    end
+
+    def serialize
+      case @checked
+      when true
+        yield key.to_s, "1"
+      when false
+        yield key.to_s, "0"
+      end
+    end
+
+    def validate
+      ::Ippon::Validate::Result.new(@checked)
+    end
+  end
+
+  class List < Entry
+    def self.element_class
+      @element_class ||= options.fetch(:of)
+    end
+
+    def setup
+      @entries = {}
+    end
+
+    def from_input(input)
+      input.each_for(key) do |id|
+        entry = add(id)
+        entry.from_input(input)
+      end
+    end
+
+    def serialize(&blk)
+      @entries.each do |name, entry|
+        yield key.to_s, name
+        entry.serialize(&blk)
+      end
+    end
+
+    def validate
+      value = []
+      result = ::Ippon::Validate::Result.new(value)
+      
+      each_with_index do |entry, idx|
+        element_result = entry.validate
+        value << element_result.value
+        result.add_nested(idx, element_result)
+      end
+      
+      result
+    end
+
+    def add(id = @entries.size.to_s)
+      subkey = key[id]
+      @entries[id] = entry = self.class.element_class.new(subkey)
+    end
+
+    def each(&blk)
+      @entries.each_value(&blk)
+    end
+
+    include Enumerable
+  end
+
+  class Group < Entry
+    # Re-export common entries
+    Text = Text
+    TextList = TextList
+    Flag = Flag
+    List = List
+
+    def self.field_spec
+      if !defined?(@field_spec)
+        raise "#fields not defined"
+      end
+
+      @field_spec
+    end
+
+    def self.fields(field_spec)
+      if defined?(@field_spec)
+        raise "#fields already defined earlier"
+      end
+
+      @field_spec = field_spec
+
+      # Define getters
+      field_spec.each do |name, klass|
+        if method_defined?(name)
+          raise "cannot define field #{name.inspect} because it clashes with a method"
+        end
+
+        attr_reader name
+      end
+    end
+
+    def self.validate(&blk)
+      schema = GroupBuilder.instance_eval(&blk)
+
+      define_method(:validate) do
+        schema.validate(self)
+      end
+    end
+
+    def setup
+      @entries = []
+
+      self.class.field_spec.each do |name, klass|
+        ivar = :"@#{name}"
+        entry = klass.new(key[name])
+        instance_variable_set(ivar, entry)
+        @entries << entry
+      end
+    end
+
+    def from_input(input)
+      @entries.each do |entry|
+        entry.from_input(input)
+      end
+    end
+
+    def serialize(&blk)
+      @entries.each do |entry|
+        entry.serialize(&blk)
+      end
+    end
+  end
+
+  module GroupBuilder
+    extend ::Ippon::Validate::Builder
+    module_function
+
+    def field(name)
+      ::Ippon::Validate::Step.new do |result|
+        entry = result.value.send(name)
+        nested_result = entry.validate
+        result.add_nested(name, nested_result)
+        result.value = nested_result.value
+      end
+    end
+  end
+end
---
+++ test/test_form.rb
@@ -0,0 +1,140 @@
+require_relative 'helper'
+
+require 'ippon/form'
+require 'ippon/form_data'
+
+class TestForm < Minitest::Test
+  include Ippon::Form
+
+  class User < Group
+    fields(
+      name: Text,
+      skills: TextList,
+      is_good: Flag,
+    )
+
+    validate do
+      form(
+        name: field(:name) | trim | required,
+        skills: field(:skills) | for_each(trim | required),
+      )
+    end
+  end
+
+  def root_key
+    Ippon::FormData::DotKey.new
+  end
+
+  def input(query_string)
+    Ippon::FormData::URLEncoded.parse(query_string)
+  end
+
+  def test_basic_group
+    user = User.new(root_key)
+    user.from_input(input("name=%20Bob&skills=Programming%20&skills=Skating"))
+
+    assert_equal " Bob", user.name.value
+    assert_equal ["Programming ", "Skating"], user.skills.to_a
+    assert_nil user.is_good.checked?
+
+    result = user.validate
+    assert result.valid?
+    expected = {
+      name: "Bob",
+      skills: ["Programming", "Skating"]
+    }
+    assert_equal expected, result.value
+  end
+
+  def test_doesnt_allow_defining_fields_twice
+    klass = Class.new(Group)
+    klass.fields(name: Text)
+    assert_raises do
+      klass.fields(name: Text)
+    end
+  end
+ 
+  def test_doesnt_allow_overriding_methods
+    klass = Class.new(Group)
+    assert_raises do
+      klass.fields(to_s: Text)
+    end
+  end
+
+  def test_requires_fields
+    klass = Class.new(Group)
+    assert_raises do
+      klass.new(root_key)
+    end
+  end
+
+  class Multi < Group
+    fields(
+      users: List[of: User],
+      send_email: Flag,
+    )
+
+    validate do
+      form(
+        users: field(:users),
+        send_email: field(:send_email),
+      )
+    end
+  end
+
+  def test_multi
+    multi = Multi.new(root_key)
+    multi.from_input(input("users=0&users.0.name=Bob&users.1.name=Alice&send_email=1"))
+
+    result = multi.validate
+    assert result.valid?
+
+    value = result.value
+    assert_equal 1, value[:users].size
+    assert_equal "Bob", value[:users][0][:name]
+  end
+
+  def serialize(entry)
+    pairs = []
+    entry.serialize do |name, value|
+      pairs << [name, value]
+    end
+    pairs
+  end
+
+  def test_serialize
+    multi = Multi.new(root_key)
+
+    user = multi.users.add
+    user.name.value = "Bob"
+    user.is_good.checked = true
+
+    assert_equal [
+      ["users", "0"],
+      ["users.0.name", "Bob"],
+      ["users.0.is_good", "1"],
+    ], serialize(multi)
+  end
+
+  def test_serialize_flag
+    entry = Flag.new(root_key["a"])
+    assert_equal [], serialize(entry)
+
+    entry.checked = true
+    assert_equal [["a", "1"]], serialize(entry)
+
+    entry.checked = false
+    assert_equal [["a", "0"]], serialize(entry)
+  end
+
+  def test_serialize_text_list
+    entry = TextList.new(root_key["a"])
+    assert_equal [], serialize(entry)
+
+    entry.values << "1"
+    assert_equal [["a", "1"]], serialize(entry)
+
+    entry.values << "1"
+    assert_equal [["a", "1"], ["a", "1"]], serialize(entry)
+  end
+end
